a1aa487e415c2a96d91e23977a092b0712598f81
fadhillah-ds <85287478+fadhillah-ds@users.noreply.github.com>
2021-06-05 10:26:31 +0700

    Add files via upload

---
+++ MuhammadFadhillahArrahim_koneksidatabase/MuhammadZidanErnandiaz_KoneksiDatabase.java
@@ -0,0 +1,23 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package MuhammadZidanErnandiaz_koneksidatabase;
+//
+///**
+// *
+// * @author creat
+// */
+//public class MuhammadZidanErnandiaz_KoneksiDatabase {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//        DataSiswa a = new DataSiswa();
+//        a;
+//    }
+// LUPAKANNNNNNNN SALAH BIKN FILEEEEEEEEE AAAAAAAAAAAFJKASDNJSCJIKDASNCXZJNC<ASDL
+//}
